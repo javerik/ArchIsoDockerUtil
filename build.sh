5c4927da273a106ac9acef56ea7eb2568415261a
jonas <jonas.ahlf@automationtechnology.de>
2023-09-18 15:09:37 +0200

    Test

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 : ${PROFILE_DIR:="${ATOS_DIR}/atos_live"}
 : ${OUT_DIR:="${ATOS_DIR}/out"}
 
-echo "Create ATOS ISO"
+echo "Create ATOS ISO from ${ATOS_DIR}"
 cd $ATOS_DIR
 pacman -Syu --noconfirm
 mkarchiso -v -w $WORKDIR -o $OUT_DIR $PROFILE_DIR
